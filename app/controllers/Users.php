e753812e2dc5a94848f8f65cbcf80c644dad68a0
Malle <malle.alaru@khk.ee>
2020-01-30 14:26:29 +0200

    blog - vormi errors 29012020

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -14,22 +14,21 @@ class Users extends Controller
     }
 
     public function register(){
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-           // filtriga issue, lisame hiljem. $_POST = filter_input(INPUT_POST, FILTER_SANITIZE_STRING);
-        }
-        $data = array(
-            //trim puhastab tyhikutest
-          'name' => trim($_POST['name']),
-          'email' => trim($_POST['email']),
-          'password' => $_POST['password'],
-          'confirm_password' => $_POST['confirm_password'],
-            'name_err' => '',
-            'email_err' => '',
-            'password_err' => '',
-            'confirm_password_err' => ''
-        );
-        if (empty($data['name'])){
-            $data['name_err'] = 'Please enter your name';
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+//      (filter_input(INPUT_POST, FILTER_SANITIZE_STRING)); // to do
+            $data = array(
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => $_POST['password'],
+                'confirm_password' => $_POST['confirm_password'],
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            );
+            if(empty($data['name'])){
+                $data['name_err'] = 'Please enter the name';
+            }
         }
         $this->view('users/register', $data);
     }
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,7 +1,6 @@
 <?php
 require_once APPROOT.'/views/inc/header.php';
 ?>
-<h1>Add user
     <div class="row">
         <div class="col-md-6 mx-auto">
             <!-- form card register -->
@@ -11,24 +10,23 @@ require_once APPROOT.'/views/inc/header.php';
                     <p class="mt-2">Please fill the fields below to register</p>
                 </div>
                 <div class="card-body">
-                    <form class="form" role="form" method="post" action="<?php echo URLROOT .'/users/register';?>">
+                    <form class="form" role="form" method="post" action="<?php echo URLROOT.'/users/register';?>">
                         <div class="form-group">
                             <label for="name">Name<sup>*</sup></label>
-                            <input type="text" class="form-control <?php echo (!empty($data['name_err']))? 'is_invalid':'';?>" id="name" placeholder="Full name">
-                            //siin viga ymber teha:<span class="invalid-feedback"><?php echo $data['name_err']?></span>
+                            <input type="text" class="form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" id="name" placeholder="Full name" name="name">
+                            <?php echo (!empty($data['name_err'])) ? '<span class="invalid-feedback">'.$data['name_err'].'</span>' : '';?>
                         </div>
                         <div class="form-group">
                             <label for="email">Email<sup>*</sup></label>
-                            <input type="email" class="form-control" id="email" placeholder="Email">
-                            //kuskil siin midagi kirjutati!!!
+                            <input type="email" class="form-control" id="email" placeholder="Email" name="email">
                         </div>
                         <div class="form-group">
                             <label for="password">Password<sup>*</sup></label>
-                            <input type="password" class="form-control" id="password" placeholder="Password">
+                            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
                         </div>
                         <div class="form-group">
                             <label for="confirm_password">Verify password<sup>*</sup></label>
-                            <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password">
+                            <input type="password" class="form-control" id="confirm_password" placeholder="Confirm Password" name="confirm_password">
                         </div>
 
                         <div class="form-group">
