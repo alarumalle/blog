f8f1f934fa86c7ce3be6fe2c56d90d5ed5d62956
Malle <malle.alaru@khk.ee>
2020-02-06 11:15:12 +0200

    register and login fun

--- app/config/constants.php
+++ app/config/constants.php
@@ -13,4 +13,4 @@ define('DB_PASS', '');
 define('DB_NAME', 'blog');
 
 //salasona pikkuse konstant
-define('PASSWORD_LEN', 5);
+define('PASSWORD_LEN', 6);
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 class Users extends Controller
 {
 
@@ -10,12 +9,12 @@ class Users extends Controller
      */
     public function __construct()
     {
-        $usersModel = $this->model('User');
+        $this->userModel = $this->model('User');
     }
 
     public function register(){
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-//      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); // to do
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); // to do
             $data = array(
                 'name' => trim($_POST['name']),
                 'email' => trim($_POST['email']),
@@ -34,6 +33,8 @@ class Users extends Controller
                 $data['email_err'] = 'Please enter the email';
             } else if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
                 $data['email_err'] = 'Please enter the valid email';
+            } else if($this->userModel->findUserByEmail($data['email'])){
+                $data['email_err'] = 'Email is already taken';
             }
 
             if(empty($data['password'])){
@@ -51,10 +52,65 @@ class Users extends Controller
             }
 
             if(empty($data['name_err']) and empty($data['email_err']) and empty($data['password_err']) and empty($data['confirm_password_err'])){
-                echo 'ok';
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+                if($this->userModel->register($data)){
+                    header('Location: '.URLROOT.'/'.'users/login');
+                } else {
+                    echo ('Something went wrong');
+                }
             }
 
+        } else {
+            $data = array(
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirm_password' => '',
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            );
         }
         $this->view('users/register', $data);
+    }
+
+    public function login(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING); // to do
+            $data = array(
+                'email' => trim($_POST['email']),
+                'password' => $_POST['password'],
+                'email_err' => '',
+                'password_err' => ''
+            );
+
+            if(empty($data['email'])){
+                $data['email_err'] = 'Please enter the email';
+            }
+
+            if(empty($data['password'])){
+                $data['password_err'] = 'Please enter the password';
+            }
+
+            if(!$this->userModel->findUserByEmail($data['email'])){
+                $data['email_err'] = 'User email is not found';
+            }
+
+            if(empty($data['email_err']) and empty($data['password_err'])){
+                // ok, log in
+            } else {
+                echo ('Something went wrong');
+            }
+
+        } else {
+            $data = array(
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => ''
+            );
+        }
+        $this->view('users/login', $data);
     }
 }
